751dac76185ee85ad1d099d0cc248116f4e5b62b
Rama Adityo <89193581+ramadityo@users.noreply.github.com>
2025-03-27 07:49:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+    setTimeout(function(){
+        document.querySelector(".loader").style.opacity = "0"; 
+    }, 1000)   
+    setTimeout(function(){
+        document.querySelector(".loader").style.display = "none";
+    }, 1500)   
+
+}
